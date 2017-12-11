5b76d4f3e631a55c992c6a36de5c48feb0e107c8
Christian Schou <christian.schou17@gmail.com>
2017-12-11 10:22:15 +0100

    Added comments to logic for MainWindow.xaml.cs

--- Wpf2/MainWindow.xaml.cs
+++ Wpf2/MainWindow.xaml.cs
@@ -27,52 +27,68 @@ namespace Wpf2
 
         private void new_Click(object sender, RoutedEventArgs e)
         {
+            //Using the singleton instance and instantiating the Controler.CS
             var controller = Controler.GetInstance();
+            //Calling the addperson method in Controler.CS
             controller.AddPerson();
+            //Data for person to add
             controller.CurentPerson.FirstName = firstnameTextBox.Text;
             controller.CurentPerson.LastName = lastnameTextBox.Text;
             controller.CurentPerson.Age = int.Parse(ageTextBox.Text);
             controller.CurentPerson.TelephoneNr = telephonenrTextBox.Text;
 
+            //Update counters in application
             this.personCountInt.Text = controller.PerconCount.ToString();
             this.indexCountInt.Text = controller.PersonIndex.ToString();
         }
 
         private void up_Click(object sender, RoutedEventArgs e)
         {
+            //Using the singleton instance and instantiating the Controler.CS
             var controller = Controler.GetInstance();
+            //Asking for the next person in Controler.CS
             controller.NextPerson();
+            //Data for person to view
             firstnameTextBox.Text = controller.CurentPerson.FirstName;
             lastnameTextBox.Text = controller.CurentPerson.LastName;
             ageTextBox.Text = controller.CurentPerson.Age.ToString();
             telephonenrTextBox.Text = controller.CurentPerson.TelephoneNr;
 
+            //Update counters in application
             this.personCountInt.Text = controller.PerconCount.ToString();
             this.indexCountInt.Text = controller.PersonIndex.ToString();
         }
 
         private void delete_Click(object sender, RoutedEventArgs e)
         {
+            //Using the singleton instance and instantiating the Controler.CS
             var controller = Controler.GetInstance();
+            //Delete person in Controler.CS
             controller.DeletePerson();
+            //Data for person to delete
             controller.CurentPerson.FirstName = firstnameTextBox.Text;
             controller.CurentPerson.LastName = lastnameTextBox.Text;
             controller.CurentPerson.Age = int.Parse(ageTextBox.Text);
             controller.CurentPerson.TelephoneNr = telephonenrTextBox.Text;
 
+            //Update counters in application
             this.personCountInt.Text = controller.PerconCount.ToString();
             this.indexCountInt.Text = controller.PersonIndex.ToString();
         }
 
         private void down_Click(object sender, RoutedEventArgs e)
         {
+            //Using the singleton instance and instantiating the Controler.CS
             var controller = Controler.GetInstance();
+            //Asking for the previous person in Controler.CS
             controller.PrevPerson();
+            //Data for person to view
             firstnameTextBox.Text = controller.CurentPerson.FirstName;
             lastnameTextBox.Text = controller.CurentPerson.LastName;
             ageTextBox.Text = controller.CurentPerson.Age.ToString();
             telephonenrTextBox.Text = controller.CurentPerson.TelephoneNr;
 
+            //Update counters in application
             this.personCountInt.Text = controller.PerconCount.ToString();
             this.indexCountInt.Text = controller.PersonIndex.ToString();
         }
